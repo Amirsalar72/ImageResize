5d431329a90cea52c28059b4a0d106043c2b004c
Amirsalar <amirsalar.alipoor@gmail.com>
2023-09-23 12:53:13 +0330

    add project

---
+++ ImageResizing/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageResizing.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            foreach (string item in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[item] as HttpPostedFileBase;
+                if (file.ContentLength == 0)
+                    continue;
+                if (file.ContentLength > 0)
+                {
+                    // width + height will force size, care for distortion
+                    //Exmaple: ImageUpload imageUpload = new ImageUpload { Width = 800, Height = 700 };
+
+                    // height will increase the width proportionally
+                    //Example: ImageUpload imageUpload = new ImageUpload { Height= 600 };
+
+                    // width will increase the height proportionally
+                    ImageUpload imageUpload = new ImageUpload { Width = 900 };
+
+                    // rename, resize, and upload
+                    //return object that contains {bool Success,string ErrorMessage,string ImageName}
+                    ImageResult imageResult = imageUpload.RenameUploadFile(file);
+                    if (imageResult.Success)
+                    {
+                        //TODO: write the filename to the db
+                        Console.WriteLine(imageResult.ImageName);
+                    }
+                    else
+                    {
+                        // use imageResult.ErrorMessage to show the error
+                        ViewBag.Error = imageResult.ErrorMessage;
+                    }
+                }
+            }
+
+            return View();
+        }
+       
+    }
+    }
---
+++ ImageResizing/ResizeApp/ImageResualt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageResizing
+{
+    public class ImageResult
+    {
+        public bool Success { get; set; }
+        public string ImageName { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
